3bb6bbd2f8ed2369de6036a73a9e96f1310a0b44
Sean Sanders <sean.d.sanders@gmail.com>
2015-10-14 01:24:31 -0500

    Added members area links.

--- cofouter/views.py
+++ cofouter/views.py
@@ -3,6 +3,4 @@ from django.shortcuts import redirect, render
 
 def landing(request):
 	context = {}
-	if request.user.is_authenticated():
-		return redirect("core:dashboard")
 	return render(request, 'cof_home.html', context)
