fdfcc1a53995f7c65742790629e563a95fafed76
Sean Sanders <sean.d.sanders@gmail.com>
2015-10-13 23:46:43 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='cof-core',
+    version='0.1',
+    packages=['cofouter'],
+    include_package_data=True,
+    description='COF main outer websiter',
+    url='http://coflocal.com/',
+    author='Sean Sanders',
+    author_email='sean.d.sanders@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
